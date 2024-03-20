c0cb090fecc70dfa86beaa6d68e063ba759afed2
Eduardo Alvarez <opticoedu@gmail.com>
2024-03-20 15:20:51 -0300

    animated accordion js script

---
+++ scripts/animated-acordion.js
@@ -0,0 +1,17 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+/*This uses JavaScript to slide down the content by setting a calculated max-height,
+ depending on the panel's height on different screen sizes */
