a3baf45cc02fd1119d162d79c3c7fb4158923cd1
Eduardo Alvarez <opticoedu@gmail.com>
2024-03-20 16:13:48 -0300

    function to Create Toggleable Tabs

---
+++ scripts/tabs.js
@@ -0,0 +1,21 @@
+function openCity(evt, cityName) { //esta funcion dispara eventos con el onclick en el html
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none"; //esconde el contenido de tabcontent con display:none
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block"; 
+    evt.currentTarget.className += " active";
+  }
