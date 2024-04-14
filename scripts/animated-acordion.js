9b40f7fcbab55cb57a8e0e31243b5e353224df3f
Eduardo Alvarez <opticoedu@gmail.com>
2024-04-14 19:33:53 -0300

    added some comments

--- scripts/animated-acordion.js
+++ scripts/animated-acordion.js
@@ -1,10 +1,10 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName("accordion"); /*tomo el button*/
+var i; /*solo una var para inicializar el for loop*/
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    this.classList.toggle("active"); /*agrega la clase .active al css*/
+    var panel = this.nextElementSibling; /*le dice que abra el panel "a continuacion o sea nextSibling element"*/
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
